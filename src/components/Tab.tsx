3fac9fc7ec0a73574ba5f58c1e37653f3e525b66
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2026-02-25 13:31:19 +0530

    feat: non-active tabs

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -2,7 +2,7 @@ import type { TabProps } from '../utils/types'
 
 const Tab = ({ tabName, active, getActionFn }: TabProps) => {
     return (
-        <div className={`${active ? 'bg-gray-800 text-white' : ''} flex-1 text-center px-4 py-2 rounded-lg shadow cursor-pointer`} onClick={getActionFn}>
+        <div className={`${active ? 'bg-gray-800 text-white' : 'bg-white'} flex-1 text-center px-4 py-2 rounded-lg shadow cursor-pointer`} onClick={getActionFn}>
             <p className='capitalize'>{tabName}</p>
         </div>
     )
